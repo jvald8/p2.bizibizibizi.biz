b71b287022cc9c1ff31416af3fadee4291404fbb
Jon Ringvald <jvald8@gmail.com>
2013-11-06 09:39:12 -0800

    Hopefully the last commit, I just started a job and I can't deal with this right now

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'jvald8@gmail.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', "p2_bizibizibizi_biz"); 
+	define('DB_NAME', 'bizibizi_p2_bizibizibizi_biz'); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
